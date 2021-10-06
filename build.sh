0094b3f689f92d8759a652563077e9cdb26c32bc
Terrell Russell <terrellrussell@gmail.com>
2021-10-06 08:17:08 -0400

    more prerequisites

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
+# irods packages - see https://packages.irods.org
+apt install -y irods-externals-cmake3.11.4-0 irods-externals-clang6.0-0
+apt install -y irods-dev irods-runtime
+# build rirods
 R CMD BATCH install-prerequisites.R
 Rscript -e 'library(Rcpp); Rcpp::compileAttributes()'
 mkdir -p build
